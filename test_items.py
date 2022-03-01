6e227c64b415a8697f69a66037c513785c7d32cf
nadya.chervyakova <kraynova06@yandex.ru>
2022-03-01 17:59:29 +0300

    languages test

---
+++ test_items.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+import time
+
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_add_to_basket_button(browser):
+    browser.get(link)
+    time.sleep(3)
+    assert browser.find_element(By.CLASS_NAME, "btn-primary"), '---Нет кнопки добавить в корзину---'
+
+
+# pytest --language=es test_items.py
+# pytest --language=es
+# pytest --language=fr
+
+
